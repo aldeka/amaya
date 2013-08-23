b318cc75255c683d8e6d5024af2907eda3e30ec2
Karen Rustad <karen.rustad@gmail.com>
2013-08-22 18:16:19 -0700

    Made links point at pages

--- footer.php
+++ footer.php
@@ -18,28 +18,28 @@
             <div class="row">
               <div class="col-lg-4 col-4">
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="">Menu &ndash; Lunch</a></li>
-                    <li><a href="">Menu &ndash; Dinner</a></li>
-                    <li><a href="">Events and Specials</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>">Home</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/menu/lunch/">Menu &ndash; Lunch</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/menu/dinner/">Menu &ndash; Dinner</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/events/">Events and Specials</a></li>
                 </ul>
               </div>
 
               <div class="col-lg-4 col-4">
                 <ul>
-                    <li><a href="">Make a Reservation</a></li>
-                    <li><a href="">Order Takeout</a></li>
-                    <li><a href="">Catering Service</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/reservations/">Make a Reservation</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/takeout/">Order Takeout</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/catering/">Catering Service</a></li>
                 </ul>
               </div>
 
               <div class="col-lg-4 col-4">
                 <ul>
-                    <li><a href="">Location</a></li>
-                    <li><a href="">Hours</a></li>
+                	<li><a href="<?php echo get_template_directory_uri(); ?>/about-us/">About Us</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/about-us/#location">Location</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/about-us/#hours">Hours</a></li>
                     <!-- <li>Press</li> -->
-                    <li><a href="">About Us</a></li>
-                    <li><a href="">Contact Us</a></li>
+                    <li><a href="<?php echo get_template_directory_uri(); ?>/about-us/#contact">Contact Us</a></li>
                 </ul>
               </div>
             </div>

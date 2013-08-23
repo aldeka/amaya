5a2884c07024c37cb3cf6c24eae539351b0dfdf8
Karen Rustad <karen.rustad@gmail.com>
2013-08-22 18:21:27 -0700

    Made pages content area a minimum height

--- page.php
+++ page.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 			<?php /* The loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-				<div class="section row">
+				<div class="singleton section row">
 	              <div class="col-lg-12 col-sm-12">
 	                <div id="post-<?php the_ID(); ?>" class="highlight">
 		                <h2><?php the_title(); ?></h2>
